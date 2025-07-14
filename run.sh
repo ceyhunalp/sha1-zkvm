af8157d73f75a242cb6ea705e79a29e056cdeb53
ceyhun <enis.alp@parity.io>
2025-07-14 21:16:52 +0200

    .

--- run.sh
+++ run.sh
@@ -9,3 +9,9 @@ mkdir -p sha1-zkvm/logs
 ./run-sha1-zkvm.sh 100k -c
 ./run-sha1-zkvm.sh 1m -c
 ./run-sha1-zkvm.sh 10m -c
+
+./run-sha1-zkvm.sh 1k -g
+./run-sha1-zkvm.sh 10k -g
+./run-sha1-zkvm.sh 100k -g
+./run-sha1-zkvm.sh 1m -g
+./run-sha1-zkvm.sh 10m -g
